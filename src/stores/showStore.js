318db84ccd974b008a833a5469f984ae87c68b45
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-20 12:44:25 -0300

    ShowStore Data Chart

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -2,9 +2,21 @@ import axios from 'axios'
 import { create } from 'zustand'
 
 const showStore = create((set) => ({
-    fetchCoinMarket: async id => {
-        const resFetchCoinMarket = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`)
-        console.log(resFetchCoinMarket.data)
+    graphData: [],
+
+
+    fetchCoinMarketValue: async id => {
+        const resFetchCoinMarketValue = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=30`)
+
+        const graphData = resFetchCoinMarketValue.data.prices.map(price => {
+            const [time,p] = price;
+            const date = new Date(time).toLocaleDateString('pt-br')
+            return {
+                Date: date,
+                Price: p,
+            }
+        })
+        set({ graphData })
     }
 }))
 

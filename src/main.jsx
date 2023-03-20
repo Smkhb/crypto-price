5a520ce29e1931af2eb33025b6a773346d385d1b
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-20 10:29:32 -0300

    Setup; Files; Router;

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Show from './pages/Show'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path='/:id' element={< Show />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
